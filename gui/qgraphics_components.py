b1c924b74cfca02cbe19d34c79cf113f10616039
209450 <209450@edu.p.lodz.pl>
2019-10-18 16:15:10 +0200

    New implementation update - pixel grid

--- gui/qgraphics_components.py
+++ gui/qgraphics_components.py
@@ -1,21 +1,28 @@
+import numpy
 from PyQt5.QtCore import QRectF
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QColor, QBrush
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsRectItem, QGraphicsView
 from PyQt5 import QtCore
 
 
 class PixelGridScene(QGraphicsScene):
-    zoom = {'value': 1, 'ratio': 0.05}
+    zoom = {'value': 1, 'ratio': 0.1}
+    default_pixel_size = 50
 
     def __init__(self, graphic_view, h, w):
         super().__init__()
         self.graphic_view = graphic_view
-        self.setBackgroundBrush(QtCore.Qt.green)
-        # self.addRect(QRectF(0, 0, 50, 50), brush=QtCore.Qt.blue)
-        self.pixelQPainter = QPainter()
-        pixel = Pixel(QtCore.Qt.blue)
-        pixel.setRect(QRectF(0, 50, 100, 100))
-        self.addItem(pixel)
+        self.setBackgroundBrush(QColor(0, 0, 0, 0))
+        self.pixels = numpy.empty((h, w), dtype=Pixel)
+        self.make_pixel_grid(h, w)
+
+    def make_pixel_grid(self, h, w):
+        size = self.default_pixel_size
+        for i in range(h):
+            for j in range(w):
+                pixel = Pixel(QRectF(j * size, i * size, size, size), Pixel.default_color)
+                self.pixels[i, j] = pixel
+                self.addItem(pixel)
 
     def mousePressEvent(self, event):
         x = event.scenePos().x()
@@ -30,9 +37,9 @@ class PixelGridScene(QGraphicsScene):
         x = event.scenePos().x()
         y = event.scenePos().y()
 
-        zoom = self.zoom
         wheel_change_angle = event.delta()
 
+        zoom = self.zoom
         change_value = zoom['ratio'] if wheel_change_angle > 0 else -zoom['ratio']
         zoom_value = zoom['value'] + change_value
 
@@ -41,11 +48,16 @@ class PixelGridScene(QGraphicsScene):
 
 
 class Pixel(QGraphicsRectItem):
-    def __init__(self, qColor):
-        super().__init__()
-        self.qColor = qColor
+    default_color = QColor(79, 75, 64)
+
+    def __init__(self, rect, qColor):
+        super().__init__(rect)
+        self.change_fulfillment(qColor)
+
+    def change_fulfillment(self, color):
+        self.color = color
+        self.brush = QBrush(color)
 
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
-        # super().paint(painter, QStyleOptionGraphicsItem, widget)
-        painter.setPen(QPen(self.qColor))
+        painter.setBrush(self.brush)
         painter.drawRect(self.rect())
