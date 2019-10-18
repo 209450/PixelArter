cd6ce6d455a73b5df9c9f5694d0bbe2d680397bd
209450 <209450@edu.p.lodz.pl>
2019-10-18 13:19:26 +0200

    New implementation update - zoom out

--- gui/qgraphics_components.py
+++ gui/qgraphics_components.py
@@ -5,6 +5,7 @@ from PyQt5 import QtCore
 
 
 class PixelGridScene(QGraphicsScene):
+    zoom = {'value': 1, 'ratio': 0.05}
 
     def __init__(self, graphic_view, h, w):
         super().__init__()
@@ -23,9 +24,19 @@ class PixelGridScene(QGraphicsScene):
         print(self.itemAt(x, y, self.graphic_view.transform()))
 
     def wheelEvent(self, event):
+        self.zoom_scene(event)
+
+    def zoom_scene(self, event):
         x = event.scenePos().x()
         y = event.scenePos().y()
-        self.graphic_view.scale(5, 5)
+
+        zoom = self.zoom
+        wheel_change_angle = event.delta()
+
+        change_value = zoom['ratio'] if wheel_change_angle > 0 else -zoom['ratio']
+        zoom_value = zoom['value'] + change_value
+
+        self.graphic_view.scale(zoom_value, zoom_value)
         self.graphic_view.centerOn(x, y)
 
 
